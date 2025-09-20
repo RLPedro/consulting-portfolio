123dff064e137a88e73d7dd56ab0116a9ec146d6
Ricardo Leitão Pedro <ricardoleitaopedro@gmail.com>
2025-09-20 15:02:45 +0200

    added analytics component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Analytics } from "@vercel/analytics/next"
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
+      <Analytics />
       <Image src="/profile.jpg" alt="Ricardo Leitão Pedro" width={150} height={150} className="rounded-full mb-4" />
       <h1 className="text-4xl font-bold">Ricardo Leitão Pedro</h1>
       <p className="text-xl">Software Developer | Entrepreneur | Technologist</p>
